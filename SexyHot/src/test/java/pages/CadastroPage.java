ec5d7f762ff3805680fe7cbcbf4d7fcdb287e925
“Eduardo <ducamilo94@gmail.com>
2019-05-15 14:31:04 -0300

    last test file

--- SexyHot/src/test/java/pages/CadastroPage.java
+++ SexyHot/src/test/java/pages/CadastroPage.java
@@ -59,7 +59,7 @@ public class CadastroPage extends Hooks {
     }
 
     public void clickCaptcha ( ) throws InterruptedException {
-        driver.click("/html[1]/body[1]/div[1]/main[1]/div[3]/div[1]/div[1]/div[1]/div[1]/form[1]/div[1]/div[1]/div[1]/div[1]/iframe[1]", "xpath");
+        driver.click("teste", "id");
         Thread.sleep(2000);
 
     }
